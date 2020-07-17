81e6bfe47c0e43515409cc7b89701f9c416ee56a
RichardJd <jdr.richard@gmail.com>
2020-07-17 07:24:56 -0300

    03-Variables

---
+++ 03 - Variables/variables/src/main.rs
@@ -0,0 +1,14 @@
+const PI: f64 = 3.14159;
+
+fn main() {
+    let mut x = 5;
+    println!("O valor de x é {}", x);
+
+    x = 7;
+    println!("O valor de x é {}", x);
+
+    let x = "Teste";
+    println!("O valor de x é {}", x);
+
+    println!("O valor da constante PI é {}", PI);
+}
